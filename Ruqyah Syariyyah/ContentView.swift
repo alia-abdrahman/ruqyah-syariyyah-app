e7cf2f462348a014712847f0e74df281669deca6
alia-abdrahman <nuralia.abdrahman@gmail.com>
2026-02-25 12:40:17 +0800

    Initial Commit

---
+++ Ruqyah Syariyyah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ruqyah Syariyyah
+//
+//  Created by Nur Alia Abd Rahman on 25/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ruqyah Syariyyah/Ruqyah_SyariyyahApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ruqyah_SyariyyahApp.swift
+//  Ruqyah Syariyyah
+//
+//  Created by Nur Alia Abd Rahman on 25/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Ruqyah_SyariyyahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
